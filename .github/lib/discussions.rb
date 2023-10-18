a39573030afcd2b9fe636d796803bd8a6481f88a
Mike Griffin <mgriffin@github.com>
2023-10-18 17:59:08 +0100

    Also act on discussions labelled stale for now

--- .github/lib/discussions.rb
+++ .github/lib/discussions.rb
@@ -57,7 +57,7 @@ Discussion = Struct.new(
       .reject { |r| r["closed"] }
       .reject { |r| r["locked"] }
       .reject { |r| r.dig("comments", "totalCount") > 0 && Date.parse(r.dig("comments", "nodes", 0, "createdAt")).after?(cutoff_date) }
-      .reject { |r| r.dig("labels", "nodes").map { |l| l["name"] }.include?("stale") }
+      #.reject { |r| r.dig("labels", "nodes").map { |l| l["name"] }.include?("stale") }
       .map do |c|
         Discussion.new(
           c["id"],
