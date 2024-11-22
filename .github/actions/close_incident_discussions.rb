5f3514dca71dfc3eb2cfd8a1d7608e195b42d440
Alida W <alidacodes@github.com>
2024-11-22 10:35:48 -0500

    addressing feedback: refactor to return objects, not hashes

--- .github/actions/close_incident_discussions.rb
+++ .github/actions/close_incident_discussions.rb
@@ -9,27 +9,25 @@ require "active_support/core_ext/numeric/time"
 
 # this action checks for any open incident discussions older than 2 days, and then closes them
 
-discussions = Discussion.find_open_incident_discussions(owner: "community", repo: "community")
+discussions = Discussion.find_open_incident_discussions(owner: "community", repo: "community").keep_if { |d| d.created_at < 2.days.ago }
 
-discussion_ids = discussions.keep_if { |d| Time.parse(d["createdAt"]) < 2.days.ago }.map! { |d| d["id"] }
-
-if discussion_ids.length == 0
+if discussions.length == 0
   puts "No discussion IDs provided, exiting"
   exit
 end
 
-discussion_ids.each do |d_id|
+discussions.each do |d|
   # if a public summary has not been provided, find the most recent incident comment and mark it as the answer
-  unless Discussion.is_answered?(id: d_id)
-    comment_id = Discussion.find_most_recent_incident_comment_id(id: d_id, actor_login: "github-actions")
+  unless d.is_answered?
+    comment_id = d.find_most_recent_incident_comment_id(actor_login: "github-actions")
 
     unless comment_id.nil?
       Discussion.mark_comment_as_answer(comment_id:)
     end
 
-    updated_body = "![A dark background with two security-themed abstract shapes positioned in the top left and bottom right corners. In the center of the image, bold white text reads \\\"Incident Resolved\\\" with a white Octocat logo.](https://github.com/community/incident-discussion-bot/blob/main/.github/src/incident_resolved.png?raw=true) \n #{Discussion.find_by_id(id: d_id)["body"]}"
-    Discussion.update_discussion(id: d_id, body: updated_body)
+    updated_body = "![A dark background with two security-themed abstract shapes positioned in the top left and bottom right corners. In the center of the image, bold white text reads \\\"Incident Resolved\\\" with a white Octocat logo.](https://github.com/community/incident-discussion-bot/blob/main/.github/src/incident_resolved.png?raw=true) \n #{d.body}"
+    d.update_discussion(body: updated_body)
   end
 
-  Discussion.close_as_resolved(id: d_id)
+  d.close_as_resolved
 end
--- .github/actions/update_incident_discussion.rb
+++ .github/actions/update_incident_discussion.rb
@@ -7,9 +7,7 @@ require_relative "../lib/discussion"
 # This script takes the context from the latest update dispatch event and updates the active incident discussion
 
 # first, we must identify the correct incident to update, in the case where there are multiple open incident discussions.
-open_discussions = Discussion.find_open_incident_discussions(owner: "community", repo: "community")
-selected_incident = open_discussions.keep_if { |d| d["body"].include?("#{ENV["INCIDENT_SLUG"]}") }.first["id"]
-discussion = Discussion.new(id: selected_incident)
+discussion = Discussion.find_open_incident_discussions(owner: "community", repo: "community").keep_if { |d| d.body.include?("#{ENV["INCIDENT_SLUG"]}") }.first
 
 # next, we need to update the discussion with the new information
 update = "### Update \n #{ENV["INCIDENT_MESSAGE"]}"
