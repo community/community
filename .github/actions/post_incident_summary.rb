7d603772b51f8504328e146a6a88c3aed71d0bf0
Alida W <alidacodes@github.com>
2024-12-09 09:12:46 -0500

    Updating logic to exit early if no discussion found

--- .github/actions/post_incident_summary.rb
+++ .github/actions/post_incident_summary.rb
@@ -9,6 +9,11 @@ require_relative "../lib/discussions"
 # first, we must identify the correct incident to update, in the case where there are multiple open incident discussions.
 selected_discussion = Discussion.find_open_incident_discussions(owner: "community", repo: "community").keep_if { |d| d.body.include?("#{ENV["INCIDENT_SLUG"]}") }.first
 
+if selected_discussion.nil?
+  puts "No applicable discussion, exiting"
+  exit
+end
+
 # add the summary as a comment to the discussion
 summary = "### Incident Summary \n #{ENV["INCIDENT_PUBLIC_SUMMARY"]}"
 comment_id = selected_discussion.add_comment(body: summary).dig("data", "addDiscussionComment", "comment", "id")
